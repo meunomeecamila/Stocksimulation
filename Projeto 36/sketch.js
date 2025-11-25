5422221ce72579e8face576f1a29a4ffe6383e3e
Camila Menezes <87725068+meunomeecamila@users.noreply.github.com>
2025-11-25 11:23:14 -0300

    Implement last fed time display and update logic
    Added functionality to read last fed time from Firebase and display it.

--- Projeto 36/sketch.js
+++ Projeto 36/sketch.js
@@ -1,75 +1,94 @@
-var dog,sadDog,happyDog, database;
-var foodS,foodStock;
+var dog, sadDog, happyDog, database;
+var foodS, foodStock;
 var addFood;
 var foodObj;
 var feed, lastFed;
 
-function preload(){
-sadDog=loadImage("Dog.png");
-happyDog=loadImage("happy dog.png");
+function preload() {
+  sadDog = loadImage("Dog.png");
+  happyDog = loadImage("happy dog.png");
 }
 
 function setup() {
-  database=firebase.database();
-  createCanvas(1000,400);
+  database = firebase.database();
+  createCanvas(1000, 400);
 
   foodObj = new Food();
 
-  foodStock=database.ref('Food');
-  foodStock.on("value",readStock);
-  
-  dog=createSprite(800,200,150,150);
-  dog.addImage(sadDog);
-  dog.scale=0.15;
+  // Lê o estoque inicial no Firebase
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
 
-  //create feed the dog button here
-  feed=createButton("Feed the dog");
+  // Lê o último horário de alimentação
+  let fedRef = database.ref('FeedTime');
+  fedRef.on("value", function (data) {
+    lastFed = data.val();
+  });
+
+  dog = createSprite(800, 200, 150, 150);
+  dog.addImage(sadDog);
+  dog.scale = 0.15;
+
+  // Botão de alimentar
+  feed = createButton("Feed the dog");
   feed.position(900, 85);
   feed.mousePressed(feedDog);
 
-  addFood=createButton("Add Food");
-  addFood.position(800,95);
+  // Botão de adicionar comida
+  addFood = createButton("Add Food");
+  addFood.position(800, 95);
   addFood.mousePressed(addFoods);
-
 }
 
 function draw() {
-  background(46,139,87);
+  background(46, 139, 87);
   foodObj.display();
 
-  //write code to read fedtime value from the database 
-  
- 
-  //write code to display text lastFed time here
+  fill(255);
+  textSize(20);
 
- 
+  // Exibe o horário da última refeição
+  if (lastFed !== undefined) {
+    if (lastFed >= 12) {
+      text("Last Feed: " + (lastFed % 12) + " PM", 50, 30);
+    } else if (lastFed === 0) {
+      text("Last Feed: 12 AM", 50, 30);
+    } else {
+      text("Last Feed: " + lastFed + " AM", 50, 30);
+    }
+  }
+
   drawSprites();
 }
 
-//function to read food Stock
-function readStock(data){
-  foodS=data.val();
+// Lê o valor de Food no banco
+function readStock(data) {
+  foodS = data.val();
   foodObj.updateFoodStock(foodS);
 }
 
-
-function feedDog(){
+// Alimenta o cachorro
+function feedDog() {
   dog.addImage(happyDog);
 
-  //write code here to update food stock and last fed time
-  foodS = foodS-1;
-  database.ref('/').update({
-    Food:foodS
-  })
-  database.ref('/').update({
-    Food:FeedTime
-  })
+  let currentStock = foodObj.getFoodStock();
+
+  if (currentStock > 0) {
+    currentStock -= 1;
+    foodObj.updateFoodStock(currentStock);
+
+    // Atualiza o estoque no Firebase
+    database.ref('/').update({
+      Food: currentStock,
+      FeedTime: hour() // salva horário correto
+    });
+  }
 }
 
-//function to add food in stock
-function addFoods(){
+// Adiciona comida ao estoque
+function addFoods() {
   foodS++;
   database.ref('/').update({
-    Food:foodS
-  })
+    Food: foodS
+  });
 }
